7d3df99dbb0810bbfff7287d3d23cc70fd3e6546
Gerson Goatache <gersongoatache@gmail.com>
2020-05-03 11:39:01 -0400

    basic views and register in url

--- innoscripta/urls.py
+++ innoscripta/urls.py
@@ -15,7 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url, include
+from rest_framework import routers
+from pizza.views import *
+
+router = routers.DefaultRouter(trailing_slash=False)
+router.register(r'orders', OrderView)
+router.register(r'users', UserView, basename='Users')
+router.register(r'pizza-info', PizzaInfoList)
+router.register(r'ingredients', IngerdientsList)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
 ]
--- pizza/views.py
+++ pizza/views.py
@@ -1,3 +1,55 @@
 from django.shortcuts import render
+from pizza.models import *
+from rest_framework import serializers, viewsets
+from rest_framework import status
+from rest_framework.response import Response
 
-# Create your views here.
+class OrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Order
+
+# ViewSets define the view behavior.
+class OrderView(viewsets.ModelViewSet):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+
+class PizzaInfoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = PizzaInfo
+
+class UserView(viewsets.ModelViewSet):
+
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    @classmethod
+    def get_extra_actions(cls):
+        return []
+
+    def get(self, request, format=None):
+        users = self.queryset
+        serializer = self.serializer_class(users, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class PizzaInfoList(viewsets.ModelViewSet):
+    queryset = PizzaInfo.objects.all()
+    serializer_class = PizzaInfoSerializer
+
+class IngerdientSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Ingredient
+
+class IngerdientsList(viewsets.ModelViewSet):
+    queryset = Ingredient.objects.all()
+    serializer_class = IngerdientSerializer
